368285ddcf09947905006ecf945faa63aebb06bb
tforkmann <tforkmann@gmail.com>
2020-10-28 12:45:17 +0100

    inital commit

---
+++ src/SqlClientProvider.Runtime/SqlClientProvider.Runtime.fs
@@ -0,0 +1,19 @@
+namespace MyNamespace
+
+open System
+
+// Put any utilities here
+[<AutoOpen>]
+module internal Utilities = 
+
+    let x = 1
+
+// Put any runtime constructs here
+type DataSource(filename:string) = 
+    member this.FileName = filename
+
+
+// Put the TypeProviderAssemblyAttribute in the runtime DLL, pointing to the design-time DLL
+[<assembly:CompilerServices.TypeProviderAssembly("SqlClientProvider.DesignTime.dll")>]
+do ()
+  
